753649f314faa9d59b9b6315cba88e4202aab1d7
ajvarga <alexanderjvargas7@gmail.com>
2020-01-17 22:11:07 -0800

    Skeleton
    Created android project and initialized 5 screens

---
+++ blt/app/src/main/java/cse118/example/blt/MainActivity.java
@@ -0,0 +1,15 @@
+package cse118.example.blt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //TODO login
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ blt/app/src/main/java/cse118/example/blt/MatchesActivity.java
@@ -0,0 +1,14 @@
+package cse118.example.blt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matches);
+    }
+}
