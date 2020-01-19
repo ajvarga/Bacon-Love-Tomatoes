befd8b9dda7ef477fbdd96983dff39910774be55
ajvarga <alexanderjvargas7@gmail.com>
2020-01-18 18:27:04 -0800

    login UI
    login page filled out, just need todo login verification

--- blt/app/src/main/java/cse118/example/blt/MainActivity.java
+++ blt/app/src/main/java/cse118/example/blt/MainActivity.java
@@ -2,14 +2,40 @@ package cse118.example.blt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    //TODO login
+    //TODO login verification
+    EditText email;
+    EditText password;
+    Button login;
+    Button createProfile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        createProfile = findViewById(R.id.createProfile);
+
+        createProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, CreateProfile.class);
+            }
+        });
+
+        login = findViewById(R.id.login);
+        //login verification
+
+
+
     }
 }
